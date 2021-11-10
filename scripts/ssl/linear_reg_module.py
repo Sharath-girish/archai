3038c3c3240144e48ad346d1b1aa96ac42ca057e
Sharath Girish <sgirish@terpmail.umd.edu>
2021-11-10 15:53:57 -0500

    imagenet eval

--- scripts/ssl/linear_reg_module.py
+++ scripts/ssl/linear_reg_module.py
@@ -83,6 +83,7 @@ parser.add_argument('--n_epochs', type=int, default=300)
 parser.add_argument('--manual_seed', type=int, default=None)
 parser.add_argument('--init_lr', type=float, default=0.05)
 parser.add_argument('--lr_schedule_type', type=str, default='cosine')
+parser.add_argument('--dataset', type=str, default='cifar10')
 
 parser.add_argument('--train_batch_size', type=int, default=256)
 parser.add_argument('--test_batch_size', type=int, default=500)
@@ -236,9 +237,37 @@ def linear_reg(features):
     kf = KFold(n_splits=args.kfolds)
     best_params = {'weight_decay':0.0, 'batch_size':128, 'init_lr':5.0}
     best_acc = 0.0
-    for weight_decay in [1e-5, 1e-4, 1e-3]:
-        for batch_size in [128]:
-            for init_lr in [5.0]:
+    if args.dataset != 'imagenet':
+        for weight_decay in [0.0, 1e-5, 1e-4, 1e-3]:
+            for batch_size in [128]:
+                for init_lr in [5.0]:
+                    args.weight_decay = weight_decay
+                    args.train_batch_size = batch_size
+                    args.init_lr = init_lr
+                    avg_acc = 0.0
+                    for train_index, val_index in kf.split(features['Xtrain']):
+                        X_train, X_val = features['Xtrain'][train_index], features['Xtrain'][val_index]
+                        y_train, y_val = features['Ytrain'][train_index], features['Ytrain'][val_index]
+                        if args.manual_seed is not None:
+                            setup_cuda(args.manual_seed)
+                        model = LinearModel(dim, num_classes)
+                        model.to(device)
+                        top1_train, top5_train, top1_val, top5_val = train(args, X_train, y_train, X_val, y_val, model, print_logs=False)
+                        avg_acc += top1_val
+                    avg_acc = avg_acc/args.kfolds
+                    cur_params = {'weight_decay':weight_decay, 'batch_size':batch_size, 'init_lr':init_lr}
+                    print('Weight decay ', weight_decay, 'Batch Size ', batch_size, 'Init LR', init_lr, 'Accuracy: ',avg_acc)
+                    if avg_acc > best_acc:
+                        best_acc = avg_acc
+                        best_params = cur_params
+                        print('Current best params: ',best_params)
+                    
+    args.weight_decay = best_params['weight_decay']
+    best_params = {'weight_decay':args.weight_decay, 'batch_size':128, 'init_lr':5.0}
+    best_acc = 0.0
+    for weight_decay in [args.weight_decay]:
+        for batch_size in [128, 256, 512]:
+            for init_lr in [5.0, 20.0]:
                 args.weight_decay = weight_decay
                 args.train_batch_size = batch_size
                 args.init_lr = init_lr
@@ -259,33 +288,6 @@ def linear_reg(features):
                     best_acc = avg_acc
                     best_params = cur_params
                     print('Current best params: ',best_params)
-                    
-    # args.weight_decay = best_params['weight_decay']
-    # best_params = {'weight_decay':0.0, 'batch_size':128, 'init_lr':5.0}
-    # best_acc = 0.0
-    # for weight_decay in [args.weight_decay]:
-    #     for batch_size in [128]:
-    #         for init_lr in [5.0]:
-    #             args.weight_decay = weight_decay
-    #             args.train_batch_size = batch_size
-    #             args.init_lr = init_lr
-    #             avg_acc = 0.0
-    #             for train_index, val_index in kf.split(features['Xtrain']):
-    #                 X_train, X_val = features['Xtrain'][train_index], features['Xtrain'][val_index]
-    #                 y_train, y_val = features['Ytrain'][train_index], features['Ytrain'][val_index]
-    #                 if args.manual_seed is not None:
-    #                     setup_cuda(args.manual_seed)
-    #                 model = LinearModel(dim, num_classes)
-    #                 model.to(device)
-    #                 top1_train, top5_train, top1_val, top5_val = train(args, X_train, y_train, X_val, y_val, model, print_logs=False)
-    #                 avg_acc += top1_val
-    #             avg_acc = avg_acc/args.kfolds
-    #             cur_params = {'weight_decay':weight_decay, 'batch_size':batch_size, 'init_lr':init_lr}
-    #             print('Weight decay ', weight_decay, 'Batch Size ', batch_size, 'Init LR', init_lr, 'Accuracy: ',avg_acc)
-    #             if avg_acc > best_acc:
-    #                 best_acc = avg_acc
-    #                 best_params = cur_params
-    #                 print('Current best params: ',best_params)
 
     args.weight_decay = best_params['weight_decay']
     args.train_batch_size = best_params['batch_size']
